c8115180188a8d5552139115a8b2af8deeab5f57
La Tasha L.  Pollard <128096377+Ljpeg@users.noreply.github.com>
2025-05-02 23:33:57 +0000

    update projects

--- data/projects.ts
+++ data/projects.ts
@@ -8,35 +8,12 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Driwwwle',
+    title: 'Mastermind',
     description:
-      'Discover creative websites and developers. A portal for you to share your projects.',
+      'Engineered a Flask-based backend, implementing game logic, API endpoints, and state management.',
     logo: '/logos/driwwwle.svg',
     link: 'https://github.com/itsnitinr/driwwwle',
     slug: 'driwwwle',
   },
-  {
-    title: 'VSCode Portfolio',
-    description:
-      'A Visual Studio Code themed developer portfolio built with Next.js and CSS Modules.',
-    logo: '/logos/vsc.svg',
-    link: 'https://github.com/itsnitinr/vscode-portfolio',
-    slug: 'vscode-portfolio',
-  },
-  {
-    title: 'Subtrackt',
-    description:
-      'A simple and elegant way to track your subscriptions and save money.',
-    logo: '/logos/subtrackt.svg',
-    link: 'https://github.com/itsnitinr/subtrackt',
-    slug: 'subtrackt',
-  },
-  {
-    title: 'Coolify Deployments',
-    description:
-      'VSCode extension to track and deploy your Coolify applications.',
-    logo: '/logos/coolify.svg',
-    link: 'https://github.com/itsnitinr/coolify-vscode-extension',
-    slug: 'coolify-vscode-extension',
-  },
+  
 ];
